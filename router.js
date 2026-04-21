05b3feecbff9d92ceb8f2b3613bcf7dc530710d0
Ryan Lee <rol@bluetel.co.uk>
2026-04-21 21:31:48 +0100

    feat(14): Add nav link

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 export const navLinks = [
 	{name: "About", link: "/about"},
 	{name: "Meetups", link: "/meetups"},
+	{name: "Conference", link: "https://conf.techmids.org/"},
 	{name: "Blog", link: "/blog"},
 	{name: "Team", link: "/team"},
 	{name: "CoC", link: "/code-of-conduct"},
